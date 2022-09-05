21543d529ffc060d1a97ba8920a22d7b6c4cd9ed
Konrad Lubowicki <konrad@lubowicki.pl>
2022-09-05 09:58:25 +0200

    Added component BookList and changed extensions into .jsx

---
+++ front/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import BookList from "./components/BookList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome </h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
